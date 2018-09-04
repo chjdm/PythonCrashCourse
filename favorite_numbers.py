c8e0cf778536f6d89cabd96b61a93a1c5eba132a
Liu Wei <liuwee@sina.com>
2018-09-04 21:55:28 +0800

    start

---
+++ favorite_numbers.py
@@ -0,0 +1,10 @@
+favorite_numbers = {
+    'xumin': 3,
+    'liuyuxuan': 100,
+    'jipson': 7,
+    'Shelly': 33,
+    'tonada': 23
+}
+
+for name, number in favorite_numbers.items():
+    print(name.title() + "'s favorite number is " + str(number) + ".")
