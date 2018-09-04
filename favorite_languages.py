719c440070678da6649d5f6d15e1ca9c6860a2f6
Liu Wei <liuwee@sina.com>
2018-09-04 21:11:31 +0800

    start

--- favorite_languages.py
+++ favorite_languages.py
@@ -6,14 +6,18 @@ favorite_languages = {
     'jen': 'c#'
     }
 
-# for name, language in favorite_languages.items():
+# for name, language in sorted(favorite_languages.items()):
 #     print('\nname:' + name)
 #     print('language: ' + language)
 
-friends = ['peter', 'shelly', 'phil', 'liuwei']
-for name in favorite_languages.keys():
-    print(name.title())
-    if name in friends:
-        print("Hi " + name.title() +
-              ", I see you favorite language is " +
-              favorite_languages[name].title() + ".")
+# friends = ['peter', 'shelly', 'phil', 'liuwei']
+# for name in favorite_languages.keys():
+#     print(name.title())
+#     if name in friends:
+#         print("Hi " + name.title() +
+#               ", I see you favorite language is " +
+#               favorite_languages[name].title() + ".")
+
+print("The following languages have been mentioned:")
+for value in set(favorite_languages.values()):
+    print(value.title())
---
+++ friends.py
@@ -0,0 +1,7 @@
+friend = {
+    'first_name': 'wan',
+    'last_name': 'hongwei',
+    'age': 43,
+    'city': 'wuhan'
+}
+print(friend)
